642515c23bed633eeda450edff29b7f7d6c0017d
Kenny <activestealth@hotmail.com>
2022-10-12 12:11:49 -0700

    Written barebone code for all Servers. Updated Makefile. Tested all Servers and they ran successfully.

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-all: serverM.cpp
+all: serverM.cpp serverC.cpp serverEE.cpp serverCS.cpp
 	g++ -g -o serverM serverM.cpp
+	g++ -g -o serverC serverC.cpp
+	g++ -g -o serverEE serverEE.cpp
+	g++ -g -o serverCS serverCS.cpp
 
 clean:
-	rm -f *.o serverM
+	rm -f *.o serverM serverC serverEE serverCS
--- serverM.cpp
+++ serverM.cpp
@@ -15,6 +15,11 @@
 #define HOST_TCP_PORT_NUM   "25082"
 #define BACKLOG             10
 
+#define SERVERC_PORT_NUM    "21082"
+#define SERVEREE_PORT_NUM   "23082"
+#define SERVERCS_PORT_NUM   "22082"
+#define MAXBUFLEN           100
+
 void sigchld_handler(int s){
     // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
@@ -92,6 +97,55 @@ int setupTCP(char *host_port_num){
     return sockfd;
 }
 
+int setupUDP(char *host_port_num){
+	int sockfd;
+	struct addrinfo hints, *servinfo, *p;
+    int rv;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_INET; // set to AF_INET to use IPv4
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags = AI_PASSIVE; // use my IP
+
+	if ((rv = getaddrinfo(HOST_IP_ADDRESS, host_port_num, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	// loop through all the results and bind to the first we can
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+				p->ai_protocol)) == -1) {
+			perror("listener: socket");
+			continue;
+		}
+
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("listener: bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "listener: failed to bind socket\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+    return sockfd;
+}
+
+//Within tcp child socket
+//queryData: data to be forwarded from client to either ServerC, ServerEE or ServerCS
+int udpQuery(int sockfd, char *queryData, char *port)
+{
+
+}
+
 int main(void){
     int tcp_sockfd, tcp_child_fd;
     int udp_sockfd;
@@ -101,7 +155,12 @@ int main(void){
     char s[INET6_ADDRSTRLEN];
 
     //setup tcp socket
-    tcp_sockfd = setupTCP(HOST_TCP_PORT_NUM);
+    tcp_sockfd = setupTCP((char *)HOST_TCP_PORT_NUM);
+
+    //setup udp socket
+    udp_sockfd = setupUDP((char *)HOST_UDP_PORT_NUM);
+
+    printf("The main server is up and running.\n");
 
     while(1){
         sin_size = sizeof(their_addr);
@@ -113,22 +172,33 @@ int main(void){
 
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), s, sizeof(s));
         printf("server: got connection from %s\n", s);
-/*
+
         if(!fork()){ //child process here
+/*
             close(tcp_sockfd); //child socket doesn't need the parent-socket listener
             
-            //This is how we send data over in TCP
-
+            //1. This is how we send data over in TCP
             if(send(tcp_child_fd, "Hello, world!", 13, 0) == -1){
                 perror("send");
             }
+
+            //2. This is how we receive data over TCP
+            recv(.)
+*/
+
+/*
+            //3. We may have to do some processing from received (TCP) data before relaying
+
+            //4. Use udpQuery(.) to send to UDP Servers (ServerC, ServerEE, ServerCS)
+*/
             close(tcp_child_fd);
             exit(0);
         }
-*/
+
         close(tcp_child_fd); //parent continues to listen but child is done.
     }
 
+    close(tcp_sockfd);
     return 0;
 }
 
